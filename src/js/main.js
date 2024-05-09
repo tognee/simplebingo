75164d93fd92c6237852a519b55c6e3f8359ed3e
tognee <tognee64@gmail.com>
2024-05-09 23:00:54 +0200

    use a single notyf instance

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,36 @@
 import { generateBoard, restoreBoard } from './domHelper';
-import { newGame } from './game';
 import { setupDialogs } from './dialogHelper';
 import { initSettings } from './settings';
 import { checkForExportURL, getExportURL } from './exporter';
 
 import { Notyf } from 'notyf';
-
 import textFit from "textfit";
+import YAML from 'js-yaml';
+
+function newGame() {
+  const allCards = window.localStorage.getItem('allCards');
+  const bingo = YAML.load(allCards);
+  const currentBoard = {}
+  window.localStorage.setItem('marked', '[]');
+  window.localStorage.setItem('currentBoard', JSON.stringify(currentBoard));
+
+  if (!bingo || bingo.length < 25) {
+    toast.error('Not enough cards');
+    return;
+  }
+
+  // Bingo board is a 5x5 array
+  // Fill it with random cards, no repeat
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+      let card = bingo.splice(Math.floor(Math.random() * bingo.length), 1)[0];
+      currentBoard[`card${i}${j}`] = card;
+    }
+  }
+  // Reset datastore
+  window.localStorage.setItem('currentBoard', JSON.stringify(currentBoard));
+  return currentBoard;
+}
 
 const toast = new Notyf();
 
